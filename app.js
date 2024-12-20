46c6e4a3b0e507e2994b1bc9366d40def4db75ae
Meister7 <emirkarmyshev7@gmail.com>
2024-12-20 22:34:02 +0600

    add public

--- app.js
+++ app.js
@@ -1,53 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const axios = require('axios');
-// const app = express();
-//
-// const targetUrl = 'https://mobilbah.onrender.com';
-//
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.get('/redirect', async (req, res) => {
-//   const redirectCondition = false; // Измени это условие в зависимости от логики
-//   if (redirectCondition) {
-//       try {
-//     // Делаем запрос к внешнему сайту, имитируя запрос браузера
-//     const response = await fetch(targetUrl);
-//
-//     // Если статус ответа не 404, перенаправляем пользователя
-//     if (response.status !== 404) {
-//       return res.redirect(targetUrl);
-//     }
-//   } catch (error) {
-//     // Логируем детали ошибки для диагностики
-//     console.error('Произошла ошибка:', error.message);
-//     if (error.response) {
-//       console.error('Статус ошибки:', error.response.status);
-//     }
-//
-//     // Если произошла ошибка (например, 404), делаем другой редирект
-//     if (error.response && error.response.status === 404) {
-//       return res.redirect('/index.html');
-//     } else {
-//       // Обработка других ошибок и отправка ответа
-//       return res.status(500).send('Произошла ошибка на сервере');
-//     }
-//   }
-//   } else {
-//       res.redirect('/index.html'); // Перенаправляем на твой сайт
-//   }
-// });
-//
-//
-//
-// // Настройка редиректа
-//
-//
-// // Запуск сервера
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`Сервер запущен на порту ${port}`);
-// });
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
@@ -55,7 +5,7 @@ const app = express();
 
 const targetUrl = 'https://mobilbah.onrender.com';
 
-app.use(express.static(path.join(__dirname, 'untitled3')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/redirect', async (req, res) => {
   const redirectCondition = false; // Измени это условие в зависимости от логики
@@ -74,7 +24,7 @@ app.get('/redirect', async (req, res) => {
 
       // Обработка ошибки 404
       if (error.response && error.response.status === 404) {
-        return res.redirect('/index.html');
+        return res.redirect('public/index.html');
       } else {
         // Обработка других ошибок
         return res.status(500).send('Произошла ошибка на сервере');
