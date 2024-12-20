ea7458ac22f8a0919698edbe1b91e3130ea7442d
Meister7 <emirkarmyshev7@gmail.com>
2024-12-21 00:54:00 +0600

    changes in json and app.js

--- app.js
+++ app.js
@@ -7,33 +7,8 @@ const targetUrl = 'https://mobilbah.onrender.com';
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/redirect', async (req, res) => {
-  const redirectCondition = false; // Измени это условие в зависимости от логики
-
-  if (redirectCondition) {
-    try {
-      // Делаем запрос к внешнему сайту
-      const response = await axios.get(targetUrl);
-
-      // Если статус ответа не 404, перенаправляем пользователя
-      if (response.status !== 404) {
-        return res.redirect(targetUrl);
-      }
-    } catch (error) {
-      console.error('Произошла ошибка:', error.message);
-
-      // Обработка ошибки 404
-      if (error.response && error.response.status === 404) {
-        return res.redirect('public/index.html');
-      } else {
-        // Обработка других ошибок
-        return res.status(500).send('Произошла ошибка на сервере');
-      }
-    }
-  } else {
-    // Перенаправляем на локальный сайт
-    res.redirect('/index.html');
-  }
+app.get('/', async (req, res) => {
+  res.redirect('/index.html');
 });
 
 // Запуск сервера
