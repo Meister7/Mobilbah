4895ba4007f4461da9bce48d606149560c26664c
Meister7 <emirkarmyshev7@gmail.com>
2025-01-13 18:14:52 +0100

    layout 3

--- public/game.js
+++ public/game.js
@@ -73,7 +73,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         let miss = new PIXI.Sprite(!isLandscape ? missAsset : missAsset)
         miss.anchor.set(0.5)
         miss.scale.set(!isLandscape ? aspectRatio * 1.6 : aspectRatio * 1.6)
-        miss.x = !isLandscape ? innerWidth * 0.25 : innerWidth / 2
+        miss.x = !isLandscape ? innerWidth * 0.23 : innerWidth / 2
         miss.y = !isLandscape ? innerHeight * 0.11 : innerHeight * 0.15
         app.stage.addChild(miss)
 
@@ -106,7 +106,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         let score = new PIXI.Sprite(!isLandscape ? scoreAsset : scoreAsset)
         score.anchor.set(0.5)
         score.scale.set(!isLandscape ? aspectRatio * 1.6 : aspectRatio * 1.6)
-        score.x = !isLandscape ? innerWidth * 0.16 : innerWidth * 0.85
+        score.x = !isLandscape ? innerWidth * 0.23 : innerWidth * 0.85
         score.y = !isLandscape ? innerHeight * 0.17 : innerHeight * 0.15
         app.stage.addChild(score)
 
